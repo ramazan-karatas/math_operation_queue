872e0397204ab94de971a6553be813b0162a2b80
RamazanKaratas <ramazankaratas626@gmail.com>
2025-09-10 19:01:54 +0300

    thread pool tests

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,28 @@
+use std::time::Duration;
 use math_operation_queue::ThreadPool;
 fn main() {
     let pool = ThreadPool::new(3);
+    std::thread::sleep(Duration::from_secs(1));
+    pool.execute(|| {
+        println!("birinci job basladi");
 
+        println!("birinci job bitti");
+    });
 
+    pool.execute(|| {
+        println!("ikinci job basladi");
+        let fib_result = fib(29);
+        println!("fibonacci hesaplamasinin sonucu: {}",fib_result);
+
+        println!("ikinci job bitti");
+    });
+    pool.execute(|| {
+        println!("ucuncu job basladi");
+        let factorial_result = factorial(15);
+        println!("faktoriyel hesaplamasinin sonucu : {}", factorial_result);
+
+        println!("ucuncu job bitti");
+    });
 }
 
 
