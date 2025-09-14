9172f989fb985ae8ab6159ed57ec3d2bc2cd3950
RamazanKaratas <ramazankaratas626@gmail.com>
2025-09-14 14:40:14 +0300

    some comment lines added

--- src/lib.rs
+++ src/lib.rs
@@ -11,25 +11,26 @@ type Job = Box<dyn FnOnce() + Send + 'static>;
 impl ThreadPool {
     pub fn new(size: usize) -> Self { // size kadar worker olusturacak.
                                      // her worker bir thread baslatacak.
-        println!("{size} uzunlugundaki threadpool olusturuldu.");
+        println!("{size} uzunlugunda threadpool olusturuldu.");
 
         let (sender, receiver) : (mpsc::Sender<Job>, mpsc::Receiver<Job>) = mpsc::channel();
-        let receiver = Arc::new(Mutex::new(receiver));
+        let receiver = Arc::new(Mutex::new(receiver)); // receiveri threadlere dagitabilmek ve ayni anda tek bir threadin erismesini saglamak icin
+                                                                             // Arc ve Mutex ile sariyorum.
 
 
         let mut workers = Vec::with_capacity(size);
 
         for i  in 0..size {
 
-            let receiver = Arc::clone(&receiver);
+            let receiver = Arc::clone(&receiver); // receiver'a bir referans, bu for dongusu icinde her thread icin yapilacak.
 
             let t = thread::spawn(move || {
                 println!("worker {} calisiyor",i);
 
                 loop {
-                    let message = receiver.lock().unwrap().recv();
+                    let message = receiver.lock().unwrap().recv(); // receiver'i aliyor, lock ile MutexGuard donduruluyor ve daha sonra recv() ile mesajlari okuyoruz.
                     match message {
-                        Ok(job) => {
+                        Ok(job) => { // job geldiyse job alindi mesaji yaziliyor ve job yapiliyor.
                             println!("{i} numarali worker bir job aldi");
                             job();
                         },
